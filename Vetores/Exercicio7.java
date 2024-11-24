c36810e99d8ab1dd3789d2c9b03d2120d72e0ef5
Henzo Paiva <henzopaiva92@gmail.com>
2024-11-24 16:24:23 -0300

    Update Exercicio7.java

--- Vetores/Exercicio7.java
+++ Vetores/Exercicio7.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Exercicio7 {
 	public static void main(String[] args) {
 		int i, a[],b[],j,r = 1;
-		final int tam = 5;
+		final int tam = 15;
 		a = new int[tam];
 		b = new int[tam];
 		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite os 5 Numeros:");
+		System.out.println("Digite os 15 Numeros:");
 		
 		for(i=0;i<tam;i++) {
 			a[i] = ler.nextInt();
